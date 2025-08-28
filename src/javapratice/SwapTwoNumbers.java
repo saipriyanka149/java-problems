5c7730fce4a817df7f009470cfb8478f6fdb39c9
Admin <Admin@DESKTOP-1HBPCNN>
2025-08-28 08:10:12 +0530

    SwappingOfTwoNumbers

---
+++ src/javapratice/SwapTwoNumbers.java
@@ -0,0 +1,16 @@
+package javapratice;
+
+public class SwapTwoNumbers {
+
+	public static void main(String[] args) {
+		int a = 78;
+		int b = 65;
+		int temp =a;
+		 a = b;
+		 b = temp;
+	System.out.println(" swapping : " +"a = " + a + ", b = " + b);
+		
+		
+	}
+
+}
