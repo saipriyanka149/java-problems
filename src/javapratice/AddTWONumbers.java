dc15848509418f86d680e59eeef12f246190ba84
Admin <Admin@DESKTOP-1HBPCNN>
2025-08-28 07:53:21 +0530

    SumOFTwoNumbers

---
+++ src/javapratice/AddTWONumbers.java
@@ -0,0 +1,15 @@
+package javapratice;
+
+public class AddTWONumbers {
+
+	public static void main(String[] args) {
+		int a = 76;
+		int b =34;
+		 int result= a+b;
+		
+		System.out.println("Sum of two numbers " + result);
+
+	}
+
+	}
+
