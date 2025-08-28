3ca65a953575f9badbd28756c04ae941c8055ba5
Admin <Admin@DESKTOP-1HBPCNN>
2025-08-28 07:35:37 +0530

    Added SumArray program

---
+++ src/javapratice/SumArray.java
@@ -0,0 +1,14 @@
+package javapratice;
+
+public class SumArray {
+
+	public static void main(String[] args) {
+		int[] numbers = {1,2,3,4,};
+		int sum =0;
+		for (int num:numbers) {
+			sum+=num;
+		}
+		System.out.println("Sum = "+sum);
+		}
+	}
+
